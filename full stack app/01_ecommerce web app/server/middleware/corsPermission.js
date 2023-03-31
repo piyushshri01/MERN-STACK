763d6c6754bd439a79e1addb93b04cf7acc4be5f
piyush shrimal <piyushshrimal94@gmail.com>
2023-03-31 08:44:20 +0530

    Delete full stack app/01_ecommerce web app/server directory

--- full stack app/01_ecommerce web app/server/middleware/corsPermission.js
+++
@@ -1,7 +0,0 @@
-const CorsPermission = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Header", "*");
-    next();
-  };
-
-module.exports = CorsPermission;
